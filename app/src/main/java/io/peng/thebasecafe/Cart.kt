335f7860a11d742c483cbe86c2b08d57c62ee447
AlfredLotsu <alfredsklotsu@yahoo.com>
2021-07-07 19:03:26 +0000

    Added view bindings to all fragments and activities

--- app/src/main/java/io/peng/thebasecafe/Cart.kt
+++ app/src/main/java/io/peng/thebasecafe/Cart.kt
@@ -5,30 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import io.peng.thebasecafe.databinding.FragmentCartBinding
+import io.peng.thebasecafe.databinding.FragmentMainBinding
 
 class Cart : Fragment() {
+    private var navController: NavController? = null
 
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
 
         }
     }
+    private var _binding: FragmentCartBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+        _binding = FragmentCartBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        binding
     }
 
     companion object {
 
         fun newInstance(param1: String, param2: String) =
-            Cart().apply {
+            Main().apply {
                 arguments = Bundle().apply {
 
                 }
             }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
